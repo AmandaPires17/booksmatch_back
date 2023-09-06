4adfe09e1118972128ce9e9823693077b55304ab
AmandaPires17 <nanapires255@gmail.com>
2023-09-06 11:34:18 -0300

    Filtro por categoria

--- livraria/views/livro.py
+++ livraria/views/livro.py
@@ -1,10 +1,16 @@
 from livraria.models.livro import Livro
+from rest_framework import viewsets
 from rest_framework.viewsets import ModelViewSet
 
 from livraria.serializers import LivroSerializer, LivroDetailSerializer, LivroListSerializer
 
-class LivroViewSet(ModelViewSet):
+from django_filters.rest_framework import DjangoFilterBackend
+
+class LivroViewSet(viewsets.ModelViewSet):
     queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["categoria__descricao"]
 
     def get_serializer_class(self):
         if self.action == "list":
